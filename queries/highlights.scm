87a761a76d07f834911f1e68b80a3a5ed212f58f
mgramigna <mgramigna@mitre.org>
2022-12-20 19:43:08 -0500

    Add some highlights

---
+++ queries/highlights.scm
@@ -0,0 +1,42 @@
+[
+ "("
+ ")"
+] @punctuation.bracket
+
+[
+ "*"
+ "^"
+ "="
+ ":"
+ ".."
+] @punctuation.operator
+
+[
+ "#"
+] @punctuation.special
+
+[
+ "Profile"
+ "Alias"
+ "Extension"
+"Invariant"
+"Instance"
+"ValueSet"
+"CodeSystem"
+"Mapping"
+"Logical"
+"Resource"
+"Parent"
+"Title"
+"Description"
+"Id"
+; "Severity"
+; "InstanceOf"
+; "Usage"
+; "Source"
+] @keyword
+
+(sd_metadata (parent (name))) @type
+(string) @string
+(multiline_string) @string
+(path) @parameter
